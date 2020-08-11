faf02b089954a16ab56352d034d232a406fa66a2
Ahmad Anwar Azzam <ahma0324@student.eadania.dk>
2020-08-11 11:41:46 +0200

    Upload af kode og styling til interaktiv side (på forsiden)

---
+++ Interaktiv side.php
@@ -0,0 +1,123 @@
+<?php
+/*
+Template Name: Interaktiv massage side
+*/
+?>
+
+<?php get_header(); ?>
+
+<div class="body-gallery-grid">
+
+    <figure>
+
+        <section class="gallery-container">
+
+            <img src="http://ahma03245.apache.eadania.dk/wp-content/uploads/2020/08/rsz_smerte3storfarvee.png"
+                class="gallery-img">
+
+
+
+            <p class="gallery-text">Har du ondt i nakken? Så kan jeg anbefale følgende massage:
+
+                <br>
+                <br>
+
+                <a href="http://ahma03245.apache.eadania.dk/nordlys-massage/" class="massage-link">Nordlys massage</a>
+
+                </a>
+
+
+
+            </p>
+
+
+        </section>
+
+    </figure>
+
+    <figure>
+
+        <section class="gallery-container">
+
+            <img src="http://ahma03245.apache.eadania.dk/wp-content/uploads/2020/08/Øvre-ryg-smerter.png"
+                class="gallery-img ">
+
+            <p class="gallery-text">Har du smerter i den øvre del af ryggen? Så vil jeg anbefale:
+
+                <br>
+                <br>
+
+                <a href="http://ahma03245.apache.eadania.dk/triggerpunkt/">triggerpunkt</a>
+
+                <br>
+
+                eller
+
+                <br>
+
+                <a href="http://ahma03245.apache.eadania.dk/dornmetoden/">dornmetoden</a>
+                </a>
+            </p>
+
+
+
+        </section>
+
+    </figure>
+
+    <figure>
+
+        <section class="gallery-container">
+
+            <img src="http://ahma03245.apache.eadania.dk/wp-content/uploads/2020/08/Lave-del-af-ryg-smerter.png"
+                class="gallery-img">
+
+            <p class="gallery-text">Føler du at du har smerter i den nedre del af ryggen? Til dette vil jeg anbefale:
+
+                <br>
+                <br>
+
+                <a href="http://ahma03245.apache.eadania.dk/nordlys-massage/" class="massage-link">cupping massage</a>
+
+                <br>
+
+                </a>
+
+            </p>
+
+            
+
+        </section>
+
+    </figure>
+
+    <figure>
+
+        <section class="gallery-container">
+
+            <img src="http://ahma03245.apache.eadania.dk/wp-content/uploads/2020/08/Ben-smerter.png"
+                class="gallery-img ">
+
+            <p class="gallery-text">Ligger smerterne i benet, og har du svært med at gå oprejst? Så vil følgende muligvis hjælpe:
+                
+                <br>
+                <br>
+
+                <a href="http://ahma03245.apache.eadania.dk/nordlys-massage/" class="massage-link">lymfedrænage</a>
+
+                <br>
+
+                </a>
+
+            </p>
+
+            
+
+        </section>
+
+    </figure>
+
+</div>
+
+
+<?php get_footer(); ?>
